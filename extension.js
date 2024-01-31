030b491213536f37c156e9675ac94fb23d868a24
ImadIdaliouali <imadidaliouali@gmail.com>
2024-01-31 21:15:27 +0100

    Implement GitHub Trending Repositories feature

--- extension.js
+++ extension.js
@@ -1,36 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const vscode = require("vscode");
+const axios = require("axios");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+async function activate(context) {
+  const res = await axios.get("https://api.gitterapp.com/");
+  const repos = res.data.map((repo) => ({
+    label: repo.name,
+    detail: repo.description,
+    link: repo.url,
+    iconPath: vscode.Uri.parse(repo.avatar),
+  }));
+  console.log(repos);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-github-trending" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    "vscode-github-trending.githubTrending",
+    async function () {
+      const repo = await vscode.window.showQuickPick(repos, {
+        matchOnDetail: true,
+      });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-github-trending.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+      if (repo == null) return;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from GitHub Trending!');
-	});
+      vscode.env.openExternal(repo.link);
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
